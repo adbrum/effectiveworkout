9c8284e43a90c91d287cb61c20310d9ec0097073
adbrum <adbrum@outlook.com>
2016-04-16 23:53:02 +0100

    fix test usersprofiles

--- effectiveworkout/usersprofiles/tests/test_model_subscribe.py
+++ effectiveworkout/usersprofiles/tests/test_model_subscribe.py
@@ -1,16 +1,26 @@
 from datetime import datetime
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 from effectiveworkout.usersprofiles.models import UserProfile
 
 
 class UserProfileModelTest(TestCase):
     def setUp(self):
-        self.obj = UserProfile(
+        self.obj = User(
+            pk=1,
+            username='admin',
+            first_name='admin',
+            last_name='admin',
+            email='admim@admin.com',
+            password='admin'
+        )
+
+        self.obj.save()
+
+        self.obj1 = UserProfile(
+            user_id=1,
             planomensalidade_id=1,
-            nome='Zé',
-            sobrenome=' Ruela',
-            emailatleta='ruela@gmail.com',
             datainicio='2016-01-20',
             datanascimento='2016-01-20',
             idade='42',
@@ -21,14 +31,14 @@ class UserProfileModelTest(TestCase):
             ativo=True
         )
 
-        self.obj.save()
+        self.obj1.save()
 
     def test_create(self):
         self.assertTrue(UserProfile.objects.exists())
 
     def test_created_at(self):
         """UserProfile must have an auto created_at attr."""
-        self.assertIsInstance(self.obj.created_at, datetime)
+        self.assertIsInstance(self.obj1.created_at, datetime)
 
     def test_str(self):
-        self.assertEqual('Zé', str(self.obj.nome))
+        self.assertEqual('admin', str(self.obj.first_name))
